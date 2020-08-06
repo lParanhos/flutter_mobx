cb518408fe6b65e9b675bb8fdb6173de8be6c98f
lParanhos <leandro.paranhos@dextra-sw.com>
2020-08-06 13:43:21 -0300

    build runner code

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,22 +1,15 @@
 import 'package:mobx/mobx.dart';
 
-class Counter {
-  /** Dentro do contrutor devinimos uma função para a nossa ação */
-  Counter() {
-    increment = Action(_increment);
-  }
+part 'counter.g.dart';
 
-  /** Observables são como nosso estado. 
-   * Aqui inicializamos ele com o valor 0
-   */
-  Observable _count = Observable(0);
+class Counter = _Counter with _$Counter;
 
-  /** Getter para pegar o valor do nosso estado */
-  int get count => _count.value;
+abstract class _Counter with Store {
+  @observable
+  int count = 0;
 
-  Action increment;
-
-  void _increment() {
-    _count.value++;
+  @action
+  void increment() {
+    count++;
   }
 }
